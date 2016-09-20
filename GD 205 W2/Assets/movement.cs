9c62b6c71a4156b145777ad4c0010456b24a4552
Kareem Clarke <clarke.kareem3@gmail.com>
2016-09-20 12:42:11 -0400

    Board Game project
    Just some Unity Data

---
+++ GD 205 W2/Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	
+	public float move = 18;
+	private Rigidbody rig;
+	// Use this for initialization
+	void Start () {
+		rig = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hAxis = Input.GetAxis ("Horizontal");
+		float vAxis = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (hAxis, 0, vAxis) * move * Time.deltaTime;
+
+		rig.MovePosition (transform.position + movement);
+	}
+}
